a5149fbac34790cabb845c8d830e06c478aa27d0
sebmid <sebmid@live.com>
2022-01-28 00:10:55 +0100

    added files

---
+++ quotes.go
@@ -0,0 +1,22 @@
+package myquotes
+
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
+
+func helloQuote() {
+	fmt.Println(quote.Hello())
+}
+
+func glassQuote() {
+	fmt.Println(quote.Glass())
+}
+
+func goQuote() {
+	fmt.Println(quote.Go())
+}
+func optQuote() {
+	fmt.Println(quote.Opt())
+}
